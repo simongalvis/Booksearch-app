542265acc51c9ce34a15e251465e2e80228fdf1b
Simon Galvis <simongalvis9@gmail.com>
2020-09-03 08:30:44 -0400

    Initial commit
    Initialize app and Search component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+//import App styling
+import './App.css'
+
+//import components
+import './Search'
+import Search from './Search';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <main className="App">
+      <header>
+        <h1>Google Book Search</h1>
       </header>
-    </div>
+      <Search/>
+    </main>
   );
 }
 
---
+++ src/Search.js
@@ -0,0 +1,18 @@
+import React from'react';
+
+//import search styling
+import './Search.css'
+
+class Search extends React.Component{
+
+render(){
+
+
+    return <div>I am a div</div>
+}
+
+
+
+}
+
+export default Search;
